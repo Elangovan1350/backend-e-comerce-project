9939da0a1868c9d0cf2fb9d7e67d306b902bd097
Elangovan1350 <elangovan2019miss@gmail.com>
2025-10-22 21:50:30 +0530

    change6

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { Hono } from "hono";
-import { PrismaClient } from "./generated/prisma";
+import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcryptjs";
 
 const prisma = new PrismaClient();
