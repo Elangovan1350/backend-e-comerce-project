ac2aa62cc58b428a60f6ac4991178794c9489fef
Elangovan1350 <elangovan2019miss@gmail.com>
2025-10-25 00:50:20 +0530

    change14

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,14 @@ import { Hono } from "hono";
 import { cors } from "hono/cors";
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcryptjs";
-
+import jwt from "jsonwebtoken";
+import { Resend } from "resend";
 const prisma = new PrismaClient();
 const app = new Hono();
 
+const resend = new Resend("re_dAsebQyw_7jMdibaYDxqqk3vG1FFJ66ud");
+
+// CORS configuration
 app.use(
   "*",
   cors({
@@ -19,6 +23,7 @@ app.use(
 );
 const saltRounds = 10;
 
+// get all users route
 app.get("/", async (c) => {
   const users = await prisma.users.findMany();
   return c.json(
@@ -26,6 +31,8 @@ app.get("/", async (c) => {
     200
   );
 });
+
+// register route
 
 app.post("/register", async (c) => {
   try {
@@ -66,6 +73,7 @@ app.post("/register", async (c) => {
   }
 });
 
+// login route
 app.post("/login", async (c) => {
   try {
     const { email, password } = await c.req.json();
@@ -104,4 +112,94 @@ app.post("/login", async (c) => {
     });
   }
 });
+
+// change password route
+
+app.post("/change-password", async (c) => {
+  try {
+    const { email, newPassword, password } = await c.req.json();
+    const passwordCheck = await prisma.users.findUnique({
+      where: { email },
+      select: { password: true },
+    });
+    if (!passwordCheck) {
+      return c.json(
+        { message: " email is not registered ", success: false },
+        200
+      );
+    }
+    const compare = bcrypt.compareSync(password, passwordCheck.password);
+    if (!compare) {
+      return c.json(
+        {
+          message: "invalid current password",
+          success: compare,
+        },
+        200
+      );
+    }
+
+    const hash = bcrypt.hashSync(newPassword, saltRounds);
+
+    const updatedUser = await prisma.users.update({
+      where: { email },
+      data: { password: hash },
+    });
+
+    return c.json(
+      {
+        message: "password updated successfully",
+        success: true,
+      },
+      200
+    );
+  } catch (error) {
+    console.log(error + " error updating password");
+    return c.json(
+      { message: "server error password cann't be updated ", success: false },
+      501
+    );
+  }
+});
+
+// function to create JWT token
+const createToken = (email: string) => {
+  const JWT_SECRET =
+    "lkfdklf8u58925584512dsfew$#@%@df4ds65fefjewi651$#^sfds4f$%#$t";
+  const payload = { email };
+  return jwt.sign(payload, JWT_SECRET, { expiresIn: "15m" });
+};
+
+// forgot password route
+app.post("/forgot-password", async (c) => {
+  try {
+    const { email } = await c.req.json();
+    const user = await prisma.users.findUnique({
+      where: { email },
+    });
+    if (!user) {
+      return c.json(
+        { message: " email is not registered ", success: false },
+        200
+      );
+    }
+
+    // Here you would typically generate a password reset token and send it via email.
+    const token = createToken(email);
+    // Send email with token (implementation not shown)
+    resend.emails.send({
+      from: "elangov@example.com",
+      to: email,
+      subject: "Password Reset",
+      html: `<p>Click <a href="https://frontend-ecommerce-project.vercel.app/reset-password?token=${token}">here</a> to reset your password.</p>`,
+    });
+  } catch (error) {
+    console.log(error);
+    return c.json({
+      message: "server error user can't reset password ",
+      success: false,
+    });
+  }
+});
+
 export default app;
